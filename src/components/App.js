3dcc290ff83490548cc7f0189dd6ed1ef48284ea
mattperkins <github@mattperkins.com>
2018-11-05 01:00:35 +0000

    format to standard

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,16 @@
-import React, { Fragment, useState, useEffect } from 'react'
+import React, { Fragment, useState, useEffect } from "react";
 
-export default function App () {
- const [counter, setCounter] = useState(0)
+export default function App() {
+  const [counter, setCounter] = useState(0);
 
-useEffect(() => {
- document.title = `The number is now ${counter}`
-})
-return(
- <Fragment>
-  {counter}
-  <button onClick={() => setCounter(counter +1)}>Count up</button>
-  <button onClick={() => setCounter(counter -1)}>Count down</button>
- </Fragment>
-)
-
+  useEffect(() => {
+    document.title = `The number is now ${counter}`;
+  });
+  return (
+    <Fragment>
+      {counter}
+      <button onClick={() => setCounter(counter + 1)}>Count up</button>
+      <button onClick={() => setCounter(counter - 1)}>Count down</button>
+    </Fragment>
+  );
 }
